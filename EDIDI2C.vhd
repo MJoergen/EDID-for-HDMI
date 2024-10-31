80e49e9556081725368574753ff01263eae69da5
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-10-31 12:23:02 -0700

    Update EDIDI2C.vhd

--- EDIDI2C.vhd
+++ EDIDI2C.vhd
@@ -3,13 +3,16 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 USE IEEE.NUMERIC_STD_UNSIGNED.ALL;
 USE WORK.states.ALL;
+USE WORK.dataBytes.ALL;
 
 ENTITY EDIDI2C IS
     PORT(clk, enable, compI2C : IN STD_LOGIC;
          byteRCV : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
          ready : OUT STD_LOGIC := '1';
          enableI2C : OUT STD_LOGIC := '0';
+         parseReady : OUT STD_LOGIC := '0';
          instructionI2C : OUT state;
+         readData : OUT data;
          byteSend : OUT STD_LOGIC_VECTOR (7 DOWNTO 0) := (OTHERS => '0')
         );
 END ENTITY;
@@ -17,9 +20,6 @@ END ENTITY;
 ARCHITECTURE BEHAVIOR OF EDIDI2C IS
 TYPE FSM IS (IDLE, STARTI2C, SENDADDR, SENDEDID, RESTARTI2C, SENDREAD, HANDLE, STOPI2C, DONE, WAITI2C);
 SIGNAL currentFSM, returnFSM : FSM := IDLE;
-
-TYPE data IS ARRAY (0 TO 255) OF STD_LOGIC_VECTOR (7 DOWNTO 0);
-SIGNAL readData : data;
 
 SIGNAL processStart : STD_LOGIC := '0';
 
@@ -62,9 +62,17 @@ BEGIN
         WHEN HANDLE => instructionI2C <= READ;
             enableI2C <= '1';
             currentFSM <= WAITI2C;
-            returnFSM <= HANDLE;
+            IF counter = 256 THEN
+                returnFSM <= STOPI2C;
+            ELSE
+                returnFSM <= HANDLE;
+            END IF;
             counter <= counter + 1;
-            readData(counter) <= byteRCV;      
+            readData(counter) <= byteRCV; 
+        WHEN STOPI2C => instructionI2C <= STOP;
+            enableI2C <= '1';
+            currentFSM <= WAITI2C;
+            returnFSM <= DONE;     
         WHEN WAITI2C => IF NOT processStart AND NOT compI2C THEN
             processStart <= '1';
         ELSIF compI2C AND processStart THEN
@@ -72,6 +80,11 @@ BEGIN
             processStart <= '0';
             enableI2C <= '0';
         END IF;
+        WHEN DONE => parseReady <= '1';
+            ready <= '1';
+            IF NOT enable THEN
+                currentFSM <= IDLE;
+            END IF;
         END CASE;
     END IF;
 END PROCESS;
